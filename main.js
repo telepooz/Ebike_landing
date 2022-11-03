414182abe7234c5638dc3a73e24b6583c3289ea8
telepooz <voziyanov2012@yandex.ru>
2022-11-03 11:44:47 +0300

    сделан футтер

--- main.js
+++ main.js
@@ -6,4 +6,17 @@ $(function(){
         autoplay: true,
         autoplaySpeed:1300,
     });
+
+    $(".menu, .bike__column").on("click","a", function (event) {
+		event.preventDefault();
+
+		//забираем идентификатор бока с атрибута href
+		var id  = $(this).attr('href'),
+
+		//узнаем высоту от начала страницы до блока на который ссылается якорь
+			top = $(id).offset().top;
+		
+		//анимируем переход на расстояние - top за 1500 мс
+		$('body,html').animate({scrollTop: top}, 800);
+	});
 });
